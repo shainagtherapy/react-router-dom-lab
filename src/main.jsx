a47d6cdb5e131ae0a3eb3847014663f6c587da6d
Shaina G <shainagtherapy@gmail.com>
2025-08-12 10:23:21 -0700

    Setup complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
-const App = () => {
-  return <h1>Hello world!</h1>;
-};
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-export default App;
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
