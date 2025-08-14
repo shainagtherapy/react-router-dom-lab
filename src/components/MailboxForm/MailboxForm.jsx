19df11eb8aefee78c9241740fb8b975e3420e7f1
Shaina G <shainagtherapy@gmail.com>
2025-08-13 19:59:23 -0700

    Mailbox Form page fixes

--- src/components/MailboxForm/MailboxForm.jsx
+++ src/components/MailboxForm/MailboxForm.jsx
@@ -19,20 +19,24 @@ const MailboxForm = (props) => {
         navigate('/mailboxes')
     }
 
+    const handleChange = ({ target }) => {
+        setFormData({...formData, [target.name]: target.value});
+    }
+
     return (
     <main>
       <h2>New Mailbox</h2>
         <form onSubmit={handleSubmit}>
             <label htmlFor="boxOwner">Name:</label>
-            <input type="text" id="boxOwner" name="boxOwner" value={formData.boxOwner} onChange={handleSubmit} />
+            <input type="text" id="boxOwner" name="boxOwner" value={formData.boxOwner} onChange={handleChange} />
 
             <label htmlFor="boxSize">Size:</label>
-            <select name="boxSize" id="boxSize" onChange={handleSubmit}>
+            <select name="boxSize" id="boxSize" value={formData.boxSize} onChange={handleChange}>
                 <option value="small">Small</option>
                 <option value="medium">Medium</option>
                 <option value="large">Large</option> </select>
-            <button type="submit">Submit</button>
         </form>
+        <button type="submit">Submit</button>
     </main>
     )
 }
