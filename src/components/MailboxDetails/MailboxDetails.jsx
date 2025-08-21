4148c856439cefd3fb35fcbf8245ff6ba1dea317
Shaina G <shainagtherapy@gmail.com>
2025-08-20 17:46:15 -0700

    Mailbox Details update

--- src/components/MailboxDetails/MailboxDetails.jsx
+++ src/components/MailboxDetails/MailboxDetails.jsx
@@ -2,12 +2,18 @@ import { useParams } from 'react-router';
 
 
 const MailboxDetails = (props) => {
+    if (!props.selected) {
+        return (
+            <div>
+                <h1>Mailbox Not Found</h1>
+            </div>
+        )
+    }
     const { mailboxId } = useParams();
 
     const selectedBox = props.mailboxes.find((mailbox) => (
         mailbox._id === Number(mailboxId)
     ));
-    console.log('mailbox details:', selectedBox)
     
     return (
         <>
